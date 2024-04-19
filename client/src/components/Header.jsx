9d47d69f25571dd493697dcc36921ff0e83cbda3
Antony Murithi <antonymurithi51@gmail.com>
2024-04-20 01:43:48 +0300

    Header with full links

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,42 +1,89 @@
-import React from 'react';
 import { CiUser } from 'react-icons/ci';
 import { CiHeart } from 'react-icons/ci';
 import { IoBagHandleOutline } from 'react-icons/io5';
 import { CiSearch } from 'react-icons/ci';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header className="bg-[#A70000]">
-            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-                <h1 className="text-white font-bold text-sm sm:text-xl flex-wrap">
-                    <span>Brand</span>
-                    <span>Interior</span>
-                    <span>KE</span>
-                </h1>
-                <div className="flex items-center">
-                    <form className="p-3 flex text-gray-600 items-center">
-                        <input
-                            type="text"
-                            placeholder="Search for anything"
-                            className="  rounded-full border-none focus:outline bg-yellow-300 w-20 sm:px-[180px] sm:py-[15px] sm:text-gray-900 "
-                        />
-                    </form>
-                    <CiSearch className="text-white mr-3 text-2xl sm:text-4xl" />
-                </div>
+        <header className="bg-[#A70000] flex flex-col">
+            <div className="">
+                <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                    <Link to="/">
+                        <h1 className="text-white font-bold text-sm sm:text-xl flex-wrap">
+                            <span>Brand</span>
+                            <span>Interior</span>
+                            <span>KE</span>
+                        </h1>
+                    </Link>
+                    <div className="flex items-center gap-2 sm:my-4 sm:gap-4">
+                        <form className="p-3 bg-white rounded-full flex items-center">
+                            <input
+                                type="text"
+                                placeholder="Search for anything"
+                                className="bg-transparent focus:outline-none w-12 sm:w-60"
+                            />
+                        </form>
 
-                <div className="flex ml-5 gap-3 sm:gap-10">
-                    <div className="w-6 h-6 flex items-center justify-center rounded-full border border-white sm:w-10 sm:h-10 sm:border-[2px]">
-                        <CiUser className="text-white text-1xl sm:text-2xl" />
+                        <div className="hover:bg-[#790202] cursor-pointer mr-3 flex items-center justify-center hover:w-7 hover:h-7  rounded-full sm:hover:w-10 sm:hover:h-10 ">
+                            <CiSearch className="text-white text-1xl sm:text-2xl" />
+                        </div>
                     </div>
-                    <div className="w-6 h-6 flex items-center justify-center rounded-full border border-white sm:w-10 sm:h-10 sm:border-[2px]">
-                        <CiHeart className="text-white text-1xl sm:text-2xl" />
+
+                    <div className="flex justify-center ml-5 gap-3 sm:gap-10 cursor-pointer">
+                        <Link to="/sign-in">
+                            <div className="w-6 h-6 flex items-center  justify-center rounded-full border border-white sm:w-10 sm:h-10 sm:border-[2px]">
+                                <CiUser className="text-white text-1xl sm:text-2xl" />
+                            </div>
+                        </Link>
+                        <Link to="/wishlist">
+                            <div className="w-6 h-6 flex items-center justify-center rounded-full border border-white sm:w-10 sm:h-10 sm:border-[2px]">
+                                <CiHeart className="text-white text-1xl sm:text-2xl" />
+                            </div>
+                        </Link>
+                        <Link to="/cart">
+                            <button
+                                className="hidden sm:px-[35px]
+                            sm:py-[8px] rounded-full sm:flex sm:items-center gap-2 bg-[#FF5952] hover:bg-[#A70000]"
+                            >
+                                <IoBagHandleOutline className="hidden sm:text-white text-1xl sm:text-2xl sm:inline" />
+                                <h3 className="text-white text-sm">Cart</h3>
+                            </button>
+                        </Link>
                     </div>
-                    <button className="hidden sm:px-[20px] rounded-full sm:flex sm:items-center gap-2 bg-[#FF5952]">
-                        <IoBagHandleOutline className="hidden sm:text-white text-1xl sm:text-2xl sm:inline" />
-                        <h3 className="text-white text-sm">Cart</h3>
-                    </button>
                 </div>
             </div>
+
+            <hr className="mx-5 sm:mx-[100px]" />
+
+            <Link to="/products">
+                <div className="mt-[15px] text-white flex gap-3 justify-center sm:my-[25px] font-bold text-sm sm:gap-10">
+                    <h2 className="hidden hover:border-white hover:border-b-2 sm:inline">
+                        Cookware
+                    </h2>
+                    <h2 className="hidden hover:border-white hover:border-b-2 sm:inline">
+                        Dining Sets
+                    </h2>
+                    <h2 className="hidden hover:border-white hover:border-b-2 sm:inline">
+                        Soup Set
+                    </h2>
+                    <h2 className="hidden hover:border-white hover:border-b-2 sm:inline">
+                        Glassware
+                    </h2>
+                    <h2 className="hidden hover:border-white hover:border-b-2 sm:inline">
+                        Stands
+                    </h2>
+                    <h2 className="hidden hover:border-white hover:border-b-2 sm:inline">
+                        Tea Sets
+                    </h2>
+                    <h2 className="hidden hover:border-white hover:border-b-2 sm:inline">
+                        Healthy & Beauty
+                    </h2>
+                    <h2 className="hidden hover:border-white hover:border-b-2 sm:inline">
+                        Appliances
+                    </h2>
+                </div>
+            </Link>
         </header>
     );
 };
